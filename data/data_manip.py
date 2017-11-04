cdd673392ecadc37317ffa979a2b13cb26741014
Alexander Karle <akarle@umass.edu>
2017-11-04 15:43:38 -0400

    moved data_manip to data

--- data/data_manip.py
+++ data/data_manip.py
@@ -1,8 +1,7 @@
 import csv
-import os
 
 reviews = {}
-br_file = os.path.join('..', 'data', 'br.csv')
+br_file = 'br.csv'
 # Parse out the all but title, author, review in the book reviews data
 print "Parsing %s into a dictionary {'author-title': review}" % br_file
 with open(br_file, 'r') as br_f:
@@ -19,7 +18,7 @@ with open(br_file, 'r') as br_f:
     # print r
 
 summarys = {}
-summ_file = os.path.join('..', 'data', 'booksummaries.txt')
+summ_file = 'booksummaries.txt'
 print "Parsing %s into a dictionary {'author-title': review}" % summ_file
 # Parse out the title, author, plot
 with open(summ_file, 'r') as summ_f:
@@ -37,7 +36,7 @@ with open(summ_file, 'r') as summ_f:
 intersect = set(summarys.keys()) & set(reviews.keys())
 
 # Write the combined data set to file
-comb_file = os.path.join('..', 'data', 'combined.csv')
+comb_file = 'combined.csv'
 print "Writing cobined dataset %s to file" % comb_file
 with open(comb_file, 'w') as comb_f:
     writer = csv.writer(comb_f, delimiter=',')
