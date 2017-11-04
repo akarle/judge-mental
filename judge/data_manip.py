e951d72d8c41a8f225498c4bce148914c1b0a2c6
Alexander Karle <akarle@umass.edu>
2017-11-04 01:14:01 -0400

    completed dataset parsing and combination

--- judge/data_manip.py
+++ judge/data_manip.py
@@ -1,18 +1,42 @@
 import csv
 import os
 
+reviews = {}
 br_file = os.path.join('..', 'data', 'br.csv')
-br_trim = os.path.join('..', 'data', 'br-trim.csv')
-if not os.path.isfile(br_trim):
-    # Parse out the all but title, author, review in the book reviews data
-    print 'creating ', br_trim
-    with open(br_file, 'r') as brf:
-        with open(br_trim, 'w') as testf:
-            reader = csv.reader(brf, delimiter=',')
-            writer = csv.writer(testf, delimiter=',')
-            reader.next()  # remove headers
-            for row in reader:
-                writer.writerow(row[1:4])
+# Parse out the all but title, author, review in the book reviews data
+print "Parsing %s into a dictionary {'author-title': review}" % br_file
+with open(br_file, 'r') as br_f:
+    reader = csv.reader(br_f, delimiter=',')
+    reader.next()  # remove headers
+    for row in reader:
+        key = str(row[1]) + '---' + str(row[2])
+        val = row[3]
+        reviews[key] = val
 
-else:
-    print '%s already exists, not recreating' % br_trim
+# for r in reviews.iteritems():
+    # print r
+
+summarys = {}
+summ_file = os.path.join('..', 'data', 'booksummaries.txt')
+print "Parsing %s into a dictionary {'author-title': review}" % summ_file
+# Parse out the title, author, plot
+with open(summ_file, 'r') as summ_f:
+    reader = csv.reader(summ_f, delimiter='\t')
+
+    for row in reader:
+        key = str(row[2]) + '---' + str(row[3])
+        val = row[-1]
+        summarys[key] = val
+
+# Moment of truth... the overlap
+# TODO: optimize for better overlap
+intersect = set(summarys.keys()) & set(reviews.keys())
+
+# Write the combined data set to file
+comb_file = os.path.join('..', 'data', 'combined.csv')
+print "Writing cobined dataset %s to file" % comb_file
+with open(comb_file, 'w') as comb_f:
+    writer = csv.writer(comb_f, delimiter=',')
+    for key in intersect:
+        row = [key, summarys[key], str(reviews[key])]
+        writer.writerow(row)
