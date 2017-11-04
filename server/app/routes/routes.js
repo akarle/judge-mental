bbcf0403230b8b58b0a0d257e7ac0abab3a3b53b
Makenzie Schwartz <mawaschwa@gmail.com>
2017-11-04 04:36:28 -0400

    basic server evidence comms

--- server/app/routes/routes.js
+++ server/app/routes/routes.js
@@ -1,15 +1,23 @@
 var path = require('path');
 var fs = require('fs');
+var spawn = require('child_process').spawn;
 
 module.exports = function(app) {
 	app.post('/upload', (req, res) => {
 		if (!req.files)
 			return res.status(400).send("No image uploaded.");
 		console.log(req.files.file.name);
-		req.files.file.mv(path.join(__dirname+'/../../temp/image.jpg'), (err) => {
+		req.files.file.mv(path.join(__dirname+'/../../../extract/image.jpg'), (err) => {
 			if (err)
 				return res.status(500).send(err);
-			res.sendFile(path.resolve('./temp/image.jpg'));
+			console.log('got image');
+			var extract_evidence = spawn('python', ["/../../../extract/evidence.py", 
+				'/../../temp/image.jpg']);
+			console.log('waiting for evidence');
+			extract_evidence.stdout.on('data', (data) => {
+				console.log('got evidence');
+				res.send(data);
+			});
 		});
 	});
 
