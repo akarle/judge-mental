ed886503c4324ea6a2d76ed7eed36e22f8907dbc
Alexander Karle <akarle@umass.edu>
2017-11-04 16:09:48 -0400

    modularizing court for use with aws and locally

---
+++ court/aws_court.py
@@ -0,0 +1,3 @@
+from court import go_to_court
+
+go_to_court('/home/ubuntu/judge-mental/court', '/usr/bin/tesseract')
--- court/court.py
+++ court/court.py
@@ -3,40 +3,41 @@ from PIL import Image, ImageEnhance
 import pytesseract
 from enchant.checker import SpellChecker
 import sys
+import os
 
 
-def verdict(evidence):
-    """ Make a verdict (review) about evidence (plot summary) """
-    # Import Judge (clf, count_vec) combo
-    clf, cv = joblib.load('judge.pkl')
+def go_to_court(court_path, tesseract_path):
+    def verdict(evidence):
+        """ Make a verdict (review) about evidence (plot summary) """
+        # Import Judge (clf, count_vec) combo
+        clf, cv = joblib.load(os.path.join(court_path, 'judge.pkl'))
 
-    # Use cv to transform the data
-    print "Reviewing older cases (transforming summary)"
-    feats = cv.transform(evidence)
+        # Use cv to transform the data
+        print "Reviewing older cases (transforming summary)"
+        feats = cv.transform(evidence)
 
-    # Use clf to return a verdict
-    print "Order in the court! Verdict produced! (review returned)"
-    return clf.predict(feats)
+        # Use clf to return a verdict
+        print "Order in the court! Verdict produced! (review returned)"
+        return clf.predict(feats)
 
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+    image = Image.open(os.path.join(court_path, 'image.jpg'))
+    image = image.resize((image.width * 2, image.height * 2), Image.BILINEAR)
+    image = ImageEnhance.Contrast(image).enhance(5.0)
 
-image = Image.open('/home/ubuntu/judge-mental/court/image.jpg')
-image = image.resize((image.width * 2, image.height * 2), Image.BILINEAR)
-image = ImageEnhance.Contrast(image).enhance(5.0)
+    text = pytesseract.image_to_string(image)
 
-text = pytesseract.image_to_string(image)
+    chkr = SpellChecker("en_US")
+    chkr.set_text(text)
+    count = 0
+    for err in chkr:
+        count += 1
 
-chkr = SpellChecker("en_US")
-chkr.set_text(text)
-count = 0
-for err in chkr:
-    count += 1
-
-goodness = float(count) / len(text.split())
-if goodness > 0.15:
-    print "-99"
-    sys.stdout.flush()
-else:
-    print str(verdict([text])[0])
-    sys.stdout.flush()
+    goodness = float(count) / len(text.split())
+    if goodness > 0.15:
+        print "-99"
+        sys.stdout.flush()
+    else:
+        print str(verdict([text])[0])
+        sys.stdout.flush()
