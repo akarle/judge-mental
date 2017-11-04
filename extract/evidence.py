7b4f6b3a81fc2a61c735a466b3b9be8a757f03b6
Makenzie Schwartz <mawaschwa@gmail.com>
2017-11-04 03:56:25 -0400

    made extract_evidence a function

--- extract/evidence.py
+++ extract/evidence.py
@@ -7,23 +7,25 @@ from PIL import ImageEnhance
 from enchant.checker import SpellChecker
 
 
-pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
+def extract_evidence(image_name):
+    """Returns text extracted from image with image_name"""
+    pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
-image = Image.open('text4.jpg')
-image = image.resize((image.width * 2, image.height * 2), Image.BILINEAR)
-image = ImageEnhance.Contrast(image).enhance(5.0)
+    image = Image.open(image_name)
+    image = image.resize((image.width * 2, image.height * 2), Image.BILINEAR)
+    image = ImageEnhance.Contrast(image).enhance(5.0)
 
-text = pytesseract.image_to_string(image)
+    text = pytesseract.image_to_string(image)
 
-chkr = SpellChecker("en_US")
-chkr.set_text(text)
-count = 0
-for err in chkr:
-    count += 1
+    chkr = SpellChecker("en_US")
+    chkr.set_text(text)
+    count = 0
+    for err in chkr:
+        count += 1
 
-goodness = float(count) / len(text.split())
-print goodness
-if goodness > 0.15:
-    print "Bad extraction"
-else:
-    print text
+    goodness = float(count) / len(text.split())
+    print goodness
+    if goodness > 0.15:
+        return ""
+    else:
+        return text
