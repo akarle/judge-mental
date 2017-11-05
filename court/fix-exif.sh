4a80e6009d09d6b5c9a6d78407f751578fb6d705
Makenzie Schwartz <mawaschwa@gmail.com>
2017-11-04 21:06:36 -0400

    set absolute paths in fix-exif

--- court/fix-exif.sh
+++ court/fix-exif.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-exiftool -ResolutionUnit=inches image.jpg
-exiftool -Orientation=1 -n image.jpg
-convert image.jpg -rotate 90 image.jpg
+fuggit="/home/ubuntu/judge-mental/court/"
+
+exiftool -ResolutionUnit=inches /home/ubuntu/judge-mental/court/image.jpg
+exiftool -Orientation=1 -n /home/ubuntu/judge-mental/court/image.jpg
+convert /home/ubuntu/judge-mental/court/image.jpg -rotate 90 /home/ubuntu/judge-mental/court/image.jpg
